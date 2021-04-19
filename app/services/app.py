a479f0f5f3a6c1e07775a2620419a0115cc367b2
deven <devendra.jha@flex.com>
2021-04-20 00:32:45 +0530

    document updated

--- app/services/app.py
+++ app/services/app.py
@@ -10,7 +10,7 @@ app.include_router(BookStore, tags=["BookStoreServices"], prefix="/bookstore")
 async def read_root():
     return {
         "message": "Welcome book store app!",
-        "1. Get books in db ":"http://127.0.0.1:8000/bookstore/bookdetails/{id},   (example-   http://127.0.0.1:8000/bookstore/bookdetails/607c61cfeeaccff87b517330)",
+        "1. Get bookdetails ":"http://127.0.0.1:8000/bookstore/bookdetails/{id},   (example-   http://127.0.0.1:8000/bookstore/bookdetails/607c61cfeeaccff87b517330)",
         "2. Get all books in db ":"http://127.0.0.1:8000/bookstore/getallbooks/",
         "3. Get books based on count ":"http://127.0.0.1:8000/bookstore/page/{limit} ,  (example:- http://127.0.0.1:8000/bookstore/page/4)",
         "4. Add new book entry in db ":"http://127.0.0.1:8000/bookstore/addbook/",
